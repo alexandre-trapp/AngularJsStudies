a8b86a2e03704ee009940b6219f9636baf216201
alexandre-trapp <alexandre.trapp@hotmail.com>
2024-03-13 23:59:53 -0300

    add routes

--- app.js
+++ app.js
@@ -1,66 +1,29 @@
-var myApp = angular.module('myApp', ['ngMessages']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', function($scope, $log, $filter, $timeout, $filter, $http) {
+myApp.config(function($routeProvider, $locationProvider) {
 
-    $scope.name = 'trapp';
+    $locationProvider.hashPrefix('');
 
-    $scope.formattedName = $filter('uppercase')($scope.name);
-
-    $log.info($scope.name);
-    $log.info($scope.formattedName);
-
-    $scope.handle = '';
-
-    $scope.lowercasehandle = function() {
-        return $filter('lowercase')($scope.handle);
-    };
-
-    $scope.$watch('handle', function(newValue, oldValue) {
-        console.info('changed!');
-        console.info('Old: ' + oldValue);
-        console.info('New: ' + newValue);
-    });
-
-    $scope.characters = 5;
-
-    $scope.rules = [
-        { rulename: "Must be 5 characters" },
-        { rulename: "Must not be used elsewhere" },
-        { rulename: "Must be cool" }
-    ];
-
-    $scope.chuckNorrisResponse = "";
-
-    $http.get('https://api.chucknorris.io/jokes/random')
-        .then(function(result) {
-
-            $scope.chuckNorrisResponse = result.data;
-            return result;
-
-        }, function (error) {
-            console.error(error);
-        }); 
-
-    // var chuckNorrisRequest = new XMLHttpRequest(); 
-    // chuckNorrisRequest.onreadystatechange = function() {
-
-    //     $scope// change any model outside of the Angular context,
-    //     // to inform Angular of the changes by calling $apply() manually
-    //     $scope.$apply(function() {
-            
-    //         if (chuckNorrisRequest.readyState == 4 && chuckNorrisRequest.status == 200){
-
-    //             $scope.chuckNorrisResponse = JSON.parse(chuckNorrisRequest.responseText);
-    //         }
-    //     });
-    // }
-
-    // chuckNorrisRequest.open("GET", "https://api.chucknorris.io/jokes/random", true);
-    // chuckNorrisRequest.send();
-
-    // $timeout(function() {
-    //     $scope.handle = 'newTwitterHandle';
-    //     console.log('scope changed');
-
-    // }, 3000);
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
 });
+
+myApp.controller('mainController', ['$scope', '$log', 
+    function($scope, $log) {
+        $scope.name = 'main';
+    }
+]);
+
+myApp.controller('secondController', ['$scope', '$log', 
+    function($scope, $log) {
+        $scope.name = 'second';
+
+    }
+]);
